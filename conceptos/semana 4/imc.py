078e46a14c1d92cd98c89b28048408eda9823e7b
Deybis Melendez <40205683+DeybisMelendez@users.noreply.github.com>
2024-09-25 12:51:54 +0000

    2024-09-25 12:51:54

--- conceptos/semana 4/imc.py
+++ conceptos/semana 4/imc.py
@@ -17,6 +17,7 @@ def calcular_imc(peso_kg, altura_m):
     print(f"Su IMC es {IMC}, su categoría es {categoria}.")
 
 cant_personas = int(input("Ingrese la cantidad de personas"))
+
 for i in cant_personas:
     # Solicitamos al usuario peso y altura
     peso = float(input("Ingrese su peso en kg: "))
