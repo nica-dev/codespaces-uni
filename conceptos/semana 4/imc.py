9a9c432f9bdbb3b9b4cde35273151d3447b0d5fb
Deybis Melendez <40205683+DeybisMelendez@users.noreply.github.com>
2024-09-25 12:53:48 +0000

    2024-09-25 12:53:48

--- conceptos/semana 4/imc.py
+++ conceptos/semana 4/imc.py
@@ -3,18 +3,7 @@ def calcular_imc(peso_kg, altura_m):
     
     # Calculamos el IMC
     IMC = peso_kg / (altura_m ** 2)
-
-    # Por descarte la categoría sería Obesidad
-    categoria = "Obesidad"
-
-    if IMC < 18.5:
-        categoria = "Bajo peso"
-    elif IMC < 25:
-        categoria = "Normal"
-    elif IMC < 30:
-        categoria = "Sobrepeso"
-
-    print(f"Su IMC es {IMC}, su categoría es {categoria}.")
+    return IMC
 
 def main():
     """"Función principal"""
@@ -25,6 +14,7 @@ def main():
         # Solicitamos al usuario peso y altura
         peso = float(input("Ingrese su peso en kg: "))
         altura = float(input("Ingrese su altura en metros: "))
-        calcular_imc(peso, altura)
+        
+        imc = calcular_imc(peso, altura)
 
 main()
